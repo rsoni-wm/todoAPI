b523e1512807754ee3da4483ebc2a00800a0ea69
rsoni-wm <139457819+rsoni-wm@users.noreply.github.com>
2023-08-23 23:44:12 +0530

    Update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,23 +1,24 @@
 class Task
   include Mongoid::Document
-include Mongoid::Timestamps
-# include Mongoid::Paranoia
+  include Mongoid::Timestamps
   extend Enumerize
-
 
   field :title, type: String
   field :description, type: String
   enumerize :status, in: [:not_start, :start, :finish], default: :not_start
-
-  field :deleted_at, type: Time
   field :tags, type: Array, default: []
-
+  field :deleted_at, type: Time
 
   validates :title, presence: true
+
   def soft_delete
     update(deleted_at: Time.now)
   end
+
   def restore
+
     update(deleted_at: nil)
+
   end
+
 end
